bd15a1ee474ff9c82dd20e0f2d932f7b93a450ed
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-01-12 17:58:54 +0100

    [CWS] fix linting error

--- cmd/security-agent/app/runtime.go
+++ cmd/security-agent/app/runtime.go
@@ -284,8 +284,8 @@ func downloadPolicy(cmd *cobra.Command, args []string) error {
 		outputPath = args[0]
 	}
 
-	downloadUrl := fmt.Sprintf("https://api.%s/api/v2/security/cloud_workload/policy/download", site)
-	fmt.Printf("Policy download url: %s\n", downloadUrl)
+	downloadURL := fmt.Sprintf("https://api.%s/api/v2/security/cloud_workload/policy/download", site)
+	fmt.Printf("Policy download url: %s\n", downloadURL)
 	fmt.Printf("Output path:         %s\n", outputPath)
 
 	headers := map[string]string{
@@ -295,7 +295,7 @@ func downloadPolicy(cmd *cobra.Command, args []string) error {
 	}
 
 	ctx := context.Background()
-	res, err := httputils.Get(ctx, downloadUrl, headers, 10*time.Second)
+	res, err := httputils.Get(ctx, downloadURL, headers, 10*time.Second)
 	if err != nil {
 		return err
 	}
